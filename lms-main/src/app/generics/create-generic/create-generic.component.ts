b30c8b0fe098710546eee9de152fd99587dfc3b9
Fabio Yusuki <fabio.nyusuki@gmail.com>
2024-03-24 19:05:19 -0300

    #creating generics module

---
+++ lms-main/src/app/generics/create-generic/create-generic.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-generic',
+  templateUrl: './create-generic.component.html',
+  styleUrl: './create-generic.component.css'
+})
+export class CreateGenericComponent {
+
+}
---
+++ lms-main/src/app/generics/generics.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateGenericComponent } from './create-generic/create-generic.component';
+
+
+
+@NgModule({
+  declarations: [
+    CreateGenericComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GenericsModule { }
